5551559245897d3aa91f0a367ec86420ffafa04b
Ben Marshall <benleem98@yahoo.com>
2021-02-17 01:42:38 -0500

    Functions financials/news now pull and display data

--- script.js
+++ script.js
@@ -1,16 +1,54 @@
+const apiKey = "c0jn4vn48v6tjhjjqlkg";
 var input = document.querySelector('.input_text');
 const button= document.querySelector('.submit');
+
 var ticker= document.querySelector('.ticker');
 var historical_data= document.querySelector('.historical_data');
-var apiKey = "c0jn4vn48v6tjhjjqlkg";
-
+var news_container=document.querySelector('.news_container');
 
 function news(){
-    fetch('https://finnhub.io/api/v1/company-news?symbol='+input.value+'&from=2020-04-30&to=2020-05-01&token='+ apiKey)
+    fetch('https://finnhub.io/api/v1/company-news?symbol='+input.value+'&from=2021-01-01&to=2022-01-01&token='+ apiKey)
     .then(response => response.json())
     .then(data =>{
-        console.log(data);
+        var entries=Object.entries(data);
+        var values=Object.values(data);
+        console.log(values);
+        news_container.innerHTML="";
+
+        for (var i = 0; i < 30; i++){
+            var headline=document.createElement('a');
+            headline.innerHTML+=values[i].headline;
+            headline.href+=values[i].url;
+            news_container.appendChild(headline);
+
+            console.log(headline);
+
+            var datetime=document.createElement('h3');
+            var time = values[i].datetime;
+            datetime.innerHTML+=converter(time);
+            news_container.appendChild(datetime);
+
+            var summary=document.createElement('p');
+            summary.innerHTML+=values[i].summary;
+            news_container.appendChild(summary);
+        } 
     })
+    .catch(err => alert("Enter valid ticker..."));
+}
+
+function converter(time){
+    var unixtimestamp = time;
+    var months_arr = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var date = new Date(unixtimestamp*1000);
+    var year = date.getFullYear();
+    var month = months_arr[date.getMonth()];
+    var day = date.getDate();
+    var hours = date.getHours();
+    var minutes = "0" + date.getMinutes();
+    var seconds = "0" + date.getSeconds();
+
+    var convdataTime = month+'-'+day+'-'+year+' '+hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+    return convdataTime;
 }
 
 function financials(){
@@ -31,7 +69,28 @@ function financials(){
     .catch(err => alert("Enter valid ticker..."));
 }
 
+// function graph(){
+
+// }
+
 button.addEventListener('click', function(){
     news();
     financials();
 })
+
+
+// Reference deference:
+
+// button.addEventListener('click', function(){
+//     fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=imperial&appid=44e2f0995fca4a8e5234feaa245cdd67')
+//     .then(response => response.json())
+//     .then(data => {
+//         console.log(data);
+        
+//         image.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+//         cityName.innerHTML = data.name + ", " + data.sys.country;
+//         desc.innerHTML = data.weather[0].description;
+//         temp.innerHTML = Math.round(data.main.temp) + "°F";
+//     })
+//     .catch(err => alert("Please enter valid city name or zip code"));
+// })
